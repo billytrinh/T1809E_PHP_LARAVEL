0d0cabd96ccd327d3103d487a1a2a5b4b4db8082
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-08-28 18:55:38 +0700

    auth

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -10,16 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get("/quan-ly-sach","DemoController@bookList");
 Route::get("/quan-ly-tac-gia","DemoController@authorList");
-Route::get("/them-sach","DemoController@themsach");
-Route::post("/them-sach","DemoController@luusach");
 
-Route::get("/sua-sach","DemoController@suasach");
-Route::post("/sua-sach","DemoController@capnhatsach");
+Route::get("/them-sach","DemoController@themsach")->middleware("auth");
 
-Route::get("/xoa-sach/{id}","DemoController@xoasach");
+Route::group(["middleware"=> "auth"],function (){
+    Route::post("/them-sach","DemoController@luusach");
+
+    Route::get("/sua-sach","DemoController@suasach");
+    Route::post("/sua-sach","DemoController@capnhatsach");
+
+    Route::get("/xoa-sach/{id}","DemoController@xoasach");
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
